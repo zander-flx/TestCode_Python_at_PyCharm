0673e5ad138cabb36c75c72af48e289c9263662f
zander-flx <felixanderdoloso90@gmail.com>
2026-04-13 14:51:07 +0800

    Updated 04/13/26 (14:50)

--- arithmetic.py
+++ arithmetic.py
@@ -2,11 +2,18 @@ num1 = 4
 num2 = 5
 
 add =  num1 + num2
+print(f"Addition: {add}")
 sub = num1 - num2
+print(f"Subtraction: {sub}")
 mul = num1 * num2
+print(f"Multiplication: {mul}")
 divInt = num1 // num2
+print(f"Division Integer: {divInt}")
 divFloat = num1 / num2
+print(f"Division Float: {divFloat}")
 modulus = num1 % num2
+print(f"Modulus: {modulus}")
 pwr = num1 ** num2
+print(f"Power: {pwr}")
 
 
