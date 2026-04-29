b4622902853f3c798f34432342dfe3a72faed16b
zander-flx <felixanderdoloso90@gmail.com>
2026-04-29 14:13:34 +0800

    Updated 04/29/26 (13:53)

--- PrelimExam.py
+++ PrelimExam.py
@@ -1,33 +1,63 @@
 
 def meal1(amount):
-    print("Your Meal set is: Meal 1")
-    print(f"Your amount is: {amount}")
-    print(f"Your payment is: {payment}")
-    print(f"Your change is: {payment - price1 * amount}")
+    if amount < price3:
+        print("===========================")
+        print("Your amount is insufficient")
+        print("============================")
+    else:
+        print("============================")
+        print("Your Meal set is: Meal 1")
+        print(f"Your amount is: {amount}")
+        print(f"Your change is: {amount- price1}")
+        print("============================")
 
 def meal2(amount):
-    print("Your Meal set is: Meal 2")
-    print(f"Your amount is: {amount}")
-    print(f"Your payment is: {payment}")
-    print(f"Your change is: {payment - price2 * amount}")
+    if amount < price3:
+        print("===========================")
+        print("Your amount is insufficient")
+        print("============================")
+    else:
+        print("============================")
+        print("Your Meal set is: Meal 2")
+        print(f"Your amount is: {amount}")
+        print(f"Your change is: {amount - price2}")
+        print("============================")
 
 def meal3(amount):
-    print("Your Meal set is: Meal 3")
-    print(f"Your amount is: {amount}")
-    print(f"Your payment is: {payment}")
-    print(f"Your change is: {payment - price3 * amount}")
+    if amount < price3:
+        print("===========================")
+        print("Your amount is insufficient")
+        print("============================")
+    else:
+        print("============================")
+        print("Your Meal set is: Meal 3")
+        print(f"Your amount is: {amount}")
+        print(f"Your change is: {amount - price3}")
+        print("============================")
 
 def meal4(amount):
-    print("Your Meal set is: Meal 4")
-    print(f"Your amount is: {amount}")
-    print(f"Your payment is: {payment}")
-    print(f"Your change is: {payment - price4 * amount}")
+    if amount < price3:
+        print("===========================")
+        print("Your amount is insufficient")
+        print("============================")
+    else:
+        print("============================")
+        print("Your Meal set is: Meal 4")
+        print(f"Your amount is: {amount}")
+        print(f"Your change is: {amount - price4}")
+        print("============================")
 
 def meal5(amount):
-    print("Your Meal set is: Meal 5")
-    print(f"Your amount is: {amount}")
-    print(f"Your payment is: {payment}")
-    print(f"Your change is: {payment - price5 * amount}")
+    if amount < price3:
+        print("===========================")
+        print("Your amount is insufficient")
+        print("============================")
+    else:
+        print("============================")
+        print("Your Meal set is: Meal 5")
+        print(f"Your amount is: {amount}")
+        print(f"Your change is: {amount - price5}")
+        print("============================")
 
 amount = 0
 terminate = ""
@@ -41,10 +71,13 @@ price5 = 60
 while terminate != "x":
     print("Do you want to order?\nPress Enter or any key to continue\nPress x to terminate ")
     terminate = input(": ")
+    if terminate == "x":
+        break
+    print("============================")
     print("Meal 1 = 20\nMeal 2 = 30\nMeal 3 = 40\nMeal 4 = 50\nMeal 5 = 60")
+    print("============================")
     select = int(input("Enter your Meal Set: "))
     amount = int(input("Enter your amount: "))
-    payment = int(input("Enter your Payment: "))
 
     if select == 1:
         meal1(amount)
