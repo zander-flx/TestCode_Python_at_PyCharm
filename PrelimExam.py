cd90dada7e8484a4f78bfe928b4a3bbd2c8bb805
Felixander <felixanderdoloso90@gmail.com>
2026-05-30 12:53:47 +0800

    tried changing some syntax.

--- PrelimExam.py
+++ PrelimExam.py
@@ -1,65 +1,65 @@
 
-def meal1(amount):
+def meal1():
     if amount < price3:
         print("===========================")
-        print("Your amount is insufficient")
+        print("Your wallet amount is insufficient")
         print("============================")
     else:
         print("============================")
         print("Your Meal set is: Meal 1")
-        print(f"Your amount is: {amount}")
+        print(f"Your wallet amount is: {amount}")
         print(f"Your change is: {amount- price1}")
         print("============================")
 
-def meal2(amount):
+def meal2():
     if amount < price3:
         print("===========================")
-        print("Your amount is insufficient")
+        print("Your wallet amount is insufficient")
         print("============================")
     else:
         print("============================")
         print("Your Meal set is: Meal 2")
-        print(f"Your amount is: {amount}")
+        print(f"Your wallet amount is: {amount}")
         print(f"Your change is: {amount - price2}")
         print("============================")
 
-def meal3(amount):
+def meal3():
     if amount < price3:
         print("===========================")
-        print("Your amount is insufficient")
+        print("Your wallet amount is insufficient")
         print("============================")
     else:
         print("============================")
         print("Your Meal set is: Meal 3")
-        print(f"Your amount is: {amount}")
+        print(f"Your wallet amount is: {amount}")
         print(f"Your change is: {amount - price3}")
         print("============================")
 
-def meal4(amount):
+def meal4():
     if amount < price3:
         print("===========================")
-        print("Your amount is insufficient")
+        print("Your wallet amount is insufficient")
         print("============================")
     else:
         print("============================")
         print("Your Meal set is: Meal 4")
-        print(f"Your amount is: {amount}")
+        print(f"Your wallet amount is: {amount}")
         print(f"Your change is: {amount - price4}")
         print("============================")
 
-def meal5(amount):
+def meal5():
     if amount < price3:
         print("===========================")
-        print("Your amount is insufficient")
+        print("Your wallet amount is insufficient")
         print("============================")
     else:
         print("============================")
         print("Your Meal set is: Meal 5")
-        print(f"Your amount is: {amount}")
+        print(f"Your wallet amount is: {amount}")
         print(f"Your change is: {amount - price5}")
         print("============================")
 
-amount = 0
+amount = 1
 terminate = ""
 
 price1 = 20
@@ -69,7 +69,7 @@ price4 = 50
 price5 = 60
 
 while terminate != "x":
-    print("Do you want to order?\nPress Enter or any key to continue\nPress x to terminate ")
+    print("Do you want to order?\nPress any key to continue\nPress x to terminate ")
     terminate = input(": ")
     if terminate == "x":
         break
@@ -77,21 +77,19 @@ while terminate != "x":
     print("Meal 1 = 20\nMeal 2 = 30\nMeal 3 = 40\nMeal 4 = 50\nMeal 5 = 60")
     print("============================")
     select = int(input("Enter your Meal Set: "))
+    amount = int(input("Enter your wallet amount: "))
 
     if select == 1:
-        meal1(amount)
+        meal1()
     elif select == 2:
-        meal2(amount)
+        meal2()
     elif select == 3:
-        meal3(amount)
+        meal3()
     elif select == 4:
-        meal4(amount)
+        meal4()
     elif select == 5:
-        meal5(amount)
+        meal5()
     else:
         print("the menu you selected is not on the list.")
         continue
-
-    amount = int(input("Enter your amount: "))
-
 
